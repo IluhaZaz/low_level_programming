2eeb1064286ffcb84668db4d25a0010e3a175985
Ilya Zazvonov <Acer@DESKTOP-5LU3HEA>
2024-10-08 14:16:28 +0300

    fix: main_simplified.cpp removed compound operations add: main.asm completed code in nasm x64

--- lab_1/main_simplified.cpp
+++ lab_1/main_simplified.cpp
@@ -6,7 +6,7 @@ void main() {
     uint32_t state[4];
 
     printf("Enter seed for generator\n");
-    uint32_t seed;
+    uint32_t seed; //r8d
     scanf("%u", &seed);
 
     state[0] = 0;
@@ -14,23 +14,26 @@ void main() {
     state[2] = 0;
     state[3] = seed;
 
-    int i = 0;
+    int i = 0; //ecx
 
     cycle_start:
-        uint32_t t = state[3];
-        const uint32_t s = state[0];
+        uint32_t t = state[3]; //r9d
+        const uint32_t s = state[0]; //r10d
 
         state[3] = state[2];
         state[2] = state[1];
         state[1] = s;
 
-        uint32_t temp = t << 11;
+        uint32_t temp = t; //r11d
+        temp <<= 11;
 
         t ^= temp;
-        temp = t >> 8;
+        temp = t;
+        temp >>= 8;
         t ^= temp;
         t ^= s;
-        temp = s >> 19;
+        temp = s;
+        temp >>= 19;
         t ^= temp;
 
         state[0] = t;
